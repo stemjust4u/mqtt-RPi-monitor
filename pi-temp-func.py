a6af36e7d2cbf2cae246bb2d4fbfe56c841e0547
stemjust4u <stemjust4u@gmail.com>
2021-02-24 11:30:30 -0700

    add functions

---
+++ pi-temp-func.py
@@ -0,0 +1,18 @@
+from vcgencmd import Vcgencmd
+from time import sleep, strftime, time
+from os import path
+from pathlib import Path
+
+logDir = path.dirname(__file__)
+logFile = "temp-log.txt"
+Path(path.join(logDir, logFile)).touch(exist_ok = True)
+vcgm = Vcgencmd()
+
+def update_log(logDir, logFile, temp):
+    with open(path.join(logDir, logFile), "a") as log:
+        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(temp)))
+
+while True:
+    cpuTemp = vcgm.measure_temp()
+    update_log(logDir, logFile, cpuTemp)
+    sleep(1)
--- pi-temp.py
+++ pi-temp.py
@@ -3,13 +3,13 @@ from time import sleep, strftime, time
 from os import path
 from pathlib import Path
 
-if __name__ == "__main__":
-    vcgm = Vcgencmd()
-    logDir = path.dirname(__file__)
-    logFile = "temp-log.txt"
-    Path(path.join(logDir, logFile)).touch(exist_ok = True)
-    with open(path.join(logDir, logFile), "a") as log:
-        while True:
-            cpuTemp = vcgm.measure_temp()
-            log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(cpuTemp)))
-            sleep(1)
+
+vcgm = Vcgencmd()
+logDir = path.dirname(__file__)
+logFile = "temp-log.txt"
+Path(path.join(logDir, logFile)).touch(exist_ok = True)
+with open(path.join(logDir, logFile), "a") as log:
+    while True:
+        cpuTemp = vcgm.measure_temp()
+        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(cpuTemp)))
+        sleep(1)
