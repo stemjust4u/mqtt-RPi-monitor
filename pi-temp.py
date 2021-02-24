c314e47c14eb48326cf06bcacdd7e1b0c85c73b6
stemjust4u <stemjust4u@gmail.com>
2021-02-24 10:53:58 -0700

    add requirements

--- pi-temp.py
+++ pi-temp.py
@@ -1,11 +1,13 @@
 from vcgencmd import Vcgencmd
 from time import sleep, strftime, time
 from os import path
+from pathlib import Path
 
 if __name__ == "__main__":
     vcgm = Vcgencmd()
     logDir = path.dirname(__file__)
     logFile = "temp-log.txt"
+    Path(path.join(logDir, logFile)).touch(exist_ok = True)
     with open(path.join(logDir, logFile), "a") as log:
         while True:
             cpuTemp = vcgm.measure_temp()
