7febd8676647c9e62a0964b9c69309833a6e9f26
stemjust4u <stemjust4u@gmail.com>
2021-02-23 22:13:23 -0700

    Added log file

--- pi-temp.py
+++ pi-temp.py
@@ -1,5 +1,13 @@
 from vcgencmd import Vcgencmd
+from time import sleep, strftime, time
+from os import path
 
-vcgm = Vcgencmd()
-cpuTemp=vcgm.measure_temp()
-print(cpuTemp)
+if __name__ == "__main__":
+    vcgm = Vcgencmd()
+    logDir = path.dirname(__file__)
+    logFile = "temp-log.txt"
+    with open(path.join(logDir, logFile), "a") as log:
+        while True:
+            cpuTemp = vcgm.measure_temp()
+            log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(cpuTemp)))
+            sleep(1)
